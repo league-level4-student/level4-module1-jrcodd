7dbe502e67410f26816a7d4182b5c69d50c0dae7
league <league@imac-27.attlocal.net>
2019-09-10 17:00:43 -0700

    CustomButtons Horoscope and SnakeGame(IP) :smile:

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -10,8 +10,45 @@ public class CustomButtonOptionPanes {
 				options, 0);
 
 		String choice = options[input];
-		
-		//use a switch statement to do something cool for each option
-		
+
+		// use a switch statement to do something cool for each option
+		for (int i = 0; i == 0;)  {
+
+			switch (choice) {
+			case "Sunday":
+				JOptionPane.showMessageDialog(null, "it's daysun");
+				break;
+			case "Monday":
+				JOptionPane.showMessageDialog(null, "it's mandoy");
+
+				break;
+			case "Tuesday":
+				JOptionPane.showMessageDialog(null, "it's tosdey");
+
+				break;
+			case "Wednesday":
+				JOptionPane.showMessageDialog(null, "it's wanday");
+
+				break;
+			case "Thursday":
+				JOptionPane.showMessageDialog(null, "it's truck");
+				break;
+			case "Friday":
+				JOptionPane.showMessageDialog(null, "it's fidiy");
+
+				break;
+			case "Saturday":
+				JOptionPane.showMessageDialog(null, "it's sitdownday");
+
+				break;
+			default:
+
+				break;
+			}
+			input = JOptionPane.showOptionDialog(null, "Choose a day of the week", "Custom Buttons", 0, -1, null,
+					options, 0);
+			choice = options[input];
+			
+		}
 	}
 }
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -3,12 +3,161 @@ package _05_Enum_Stuff;
 import javax.swing.JOptionPane;
 
 public class _00_Horoscope {
-	// 1. Create an enum in a separate file called Zodiac that contains a category for
-	//    all 12 zodiac signs.
-	
-	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
-	//    a different horoscope based on the Zodiac's state.
-	
+	public static int input;
+	public static String choice;
+	public static String[] options = { "aries", "aquarius", "cancer", "capricorn", "capricorn", "gemini", "leo",
+			"libra", "pisces", "sagittarius", "scorpio", "taurus", "virgo" };
+	// 1. Create an enum in a separate file called Zodiac that contains a category
+	// for
+	// all 12 zodiac signs.
+
+	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane
+	// to display
+	// a different horoscope based on the Zodiac's state.
+	void display(Zodiac sign) {
+		switch (sign) {
+		case ARIES:
+			JOptionPane.showMessageDialog(null, "aeries");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case AQUARIUS:
+			JOptionPane.showMessageDialog(null, "aquarius");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case CANCER:
+			JOptionPane.showMessageDialog(null, "cancer");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case CAPRICORN:
+			JOptionPane.showMessageDialog(null, "capricorn");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case GEMINI:
+			JOptionPane.showMessageDialog(null, "gemini");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case LEO:
+			JOptionPane.showMessageDialog(null, "leo");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case LIBRA:
+			JOptionPane.showMessageDialog(null, "libra");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case PISCES:
+			JOptionPane.showMessageDialog(null, "pisces");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case SAGITTARIUS:
+			JOptionPane.showMessageDialog(null, "sagittarius");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case SCORPIO:
+			JOptionPane.showMessageDialog(null, "scorpio");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case TAURUS:
+			JOptionPane.showMessageDialog(null, "taurus");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		case VIRGO:
+			JOptionPane.showMessageDialog(null, "virgo");
+			input = JOptionPane.showOptionDialog(null, "Chose a Zodiac sign", "Zodaic", 0, -1, null,
+					options, 0);
+
+			choice = options[input];
+			break;
+		default:
+			break;
+		}
+	}
+
 	// 3. Make a main method to test your method
-	
+	public static void main(String[] args) {
+		_00_Horoscope thing = new _00_Horoscope();
+
+		input = JOptionPane.showOptionDialog(null, "Choose a Zodaic sign", "Zodaic", 0, -1, null, options,
+				0);
+
+		choice = options[input];
+		for (int i = 0; i == 0;) {
+			switch (choice) {
+			case "aries":
+				thing.display(Zodiac.ARIES);
+				break;
+			case "aquarius":
+				thing.display(Zodiac.AQUARIUS);
+				break;
+			case "cancer":
+				thing.display(Zodiac.CANCER);
+				break;
+			case "capricorn":
+				thing.display(Zodiac.CAPRICORN);
+				break;
+			case "gemini":
+				thing.display(Zodiac.GEMINI);
+				break;
+			case "leo":
+				thing.display(Zodiac.LEO);
+				break;
+			case "libra":
+				thing.display(Zodiac.LIBRA);
+				break;
+			case "pisces":
+				thing.display(Zodiac.PISCES);
+				break;
+			case "sagittarius":
+				thing.display(Zodiac.SAGITTARIUS);
+				break;
+			case "scorpio":
+				thing.display(Zodiac.SCORPIO);
+				break;
+			case "taurus":
+				thing.display(Zodiac.TAURUS);
+				break;
+			case "virgo":
+				thing.display(Zodiac.VIRGO);
+				break;
+			default:
+				break;
+			}
+		}
+
+		input = JOptionPane.showOptionDialog(null, "Choose a zodiac sign", "Zodiac Signs", 0, -1, null, options, 0);
+		choice = options[input];
+
+	}
 }
